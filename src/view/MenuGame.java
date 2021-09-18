19b97e7c43425e208a64c4f8829c33eacf879def
đttha <dothithuha140@gmail.com>
2021-09-18 21:41:37 +0700

    test

--- src/view/MenuGame.java
+++ src/view/MenuGame.java
@@ -127,6 +127,7 @@ public class MenuGame extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        Navigator.navigateTo("GameUI");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
